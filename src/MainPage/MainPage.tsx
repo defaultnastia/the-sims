fcee5fa73ebc3b04e6b1d10b65fbaf01885ca02e
defaultnastia <anastayaaa@gmail.com>
2024-08-22 13:01:26 +0200

    added some plumbobs and fonts

--- src/MainPage/MainPage.tsx
+++ src/MainPage/MainPage.tsx
@@ -1,11 +1,22 @@
+import css from ".//MainPage.module.css";
+import plumbob from "../img/plumbobMov.gif";
+import self from "../img/self.png";
+
 const MainPage = () => {
   return (
-    <>
-      <h3>The Sims UA Community</h3>
+    <div className={css.hero}>
+      <div className={css.logoContainer}>
+        <img className={css.plumbobLogo} src={plumbob} alt="moving plumbob" />
+        <h3>The Sims UA Community</h3>
+      </div>
       <h1>
         Українська спільнота гравців <span>The Sims</span>
       </h1>
-    </>
+      <div className={css.selfContainer}>
+        <img className={css.plumbob} src={plumbob} alt="moving plumbob" />
+        <img className={css.selfPic} src={self} alt="_personal_photo" />
+      </div>
+    </div>
   );
 };
 
