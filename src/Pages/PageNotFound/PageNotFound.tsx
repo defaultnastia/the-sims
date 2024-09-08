af05a9c556478e19668f7bca3b715a54c087b6ec
defaultnastia <anastayaaa@gmail.com>
2024-09-08 17:53:09 +0200

    added home link on 404

--- src/Pages/PageNotFound/PageNotFound.tsx
+++ src/Pages/PageNotFound/PageNotFound.tsx
@@ -1,15 +1,18 @@
+import { NavLink } from "react-router-dom";
 import css from "./PageNotFound.module.css";
 
 const PageNotFound = () => {
   return (
     <div className={css.notFound}>
-      <p className={css.error}>
-        E <span>r</span>ror
-      </p>
-      <p className={css.code}>
-        4<span>0</span>
-        <span>4</span>
-      </p>
+      <NavLink to="/">
+        <p className={css.error}>
+          E <span>r</span>ror
+        </p>
+        <p className={css.code}>
+          4<span>0</span>
+          <span>4</span>
+        </p>
+      </NavLink>
     </div>
   );
 };
--- src/Pages/TheSimsFour/TheSimsFour.tsx
+++ src/Pages/TheSimsFour/TheSimsFour.tsx
@@ -1,5 +1,13 @@
+import { NavLink } from "react-router-dom";
+import css from "./TheSimsFour.module.css";
+
 const TheSimsFour = () => {
-  return <div>TheSimsFour</div>;
+  return (
+    <div>
+      <NavLink to="/"> ⬅️ На головну</NavLink>
+      <h2></h2>
+    </div>
+  );
 };
 
 export default TheSimsFour;
